cd9749516287f596c8abccc0a0e158fcd202457a
nutspk <supakit.itx@gmail.com>
2020-09-23 17:03:21 +0700

    route

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('bar', function () {
+Route::post('bar', function () {
     $datas = file_get_contents('php://input');
 	    $curl = curl_init();
 
