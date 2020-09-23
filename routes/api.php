a86ed73bb183aa1d76b990ab5fa03404c5f35546
nutspk <supakit.itx@gmail.com>
2020-09-23 16:59:21 +0700

    route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,38 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('bar', function () {
+    $datas = file_get_contents('php://input');
+	    $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "165.22.246.170:8000/api/home/AddResponse",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $datas,
+        CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "content-type: application/json",
+                "postman-token: 3b9bd1e3-8803-99ed-059b-3f635e6bfcb1"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        echo $response;
+        }
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
